4ca4df380aba5b3783b00d41e414ef8d253472ce
Anto P <ganto1812@gmail.com>
2021-09-01 16:12:24 +0100

    Vanilla projects urls fixed

--- src/app/vanillaJS/vanillaJS.component.ts
+++ src/app/vanillaJS/vanillaJS.component.ts
@@ -7,16 +7,16 @@ import { Component } from '@angular/core';
 })
 export class VanillaJSComponent {
   projects = [
-    {name: "Tribute Page", src: "tribute.jpg", link:"https://ana-portfolio-vanillajs.web.app/tributepage/"},
+    {name: "Tribute Page", src: "tribute.jpg", link:"https://ana-portfolio-vanillajs.web.app/tributePage/"},
     {name: "Drum Set", src: "drum.jpg", link:"https://ana-portfolio-vanillajs.web.app/drums"},
     {name: "Clock", src: "clock.jpg", link:"https://ana-portfolio-vanillajs.web.app/clock"},
     {name: "Draw!", src: "draw.jpg", link:"https://ana-portfolio-vanillajs.web.app/draw/"},
-    {name: "Live Editing", src: "liveUpdate.jpg", link:"https://ana-portfolio-vanillajs.web.app/liveupdate/"},
-    {name: "Flex Panels", src: "flexPanels.jpg", link:"https://ana-portfolio-vanillajs.web.app/flexpanels/"},
-    {name: "Mouse Effect", src: "mouseEffects.jpg", link:"https://ana-portfolio-vanillajs.web.app/mouseeffects/"},
-    {name: "Search bar", src: "searchBar.jpg", link:"https://ana-portfolio-vanillajs.web.app/searchbar/"},
-    {name: "Slide Effect", src: "slideEffect.jpg", link:"https://ana-portfolio-vanillajs.web.app/slideeffect/"},
-    {name: "Tapas List", src: "tapasList.jpg", link:"https://ana-portfolio-vanillajs.web.app/tapaslist/"},
-    {name: "Pick a Voice", src: "voiceChanger.jpg", link:"https://ana-portfolio-vanillajs.web.app/voicechanger/"}
+    {name: "Live Editing", src: "liveUpdate.jpg", link:"https://ana-portfolio-vanillajs.web.app/liveUpdate/"},
+    {name: "Flex Panels", src: "flexPanels.jpg", link:"https://ana-portfolio-vanillajs.web.app/flexPanels/"},
+    {name: "Mouse Effect", src: "mouseEffects.jpg", link:"https://ana-portfolio-vanillajs.web.app/mouseEffects/"},
+    {name: "Search bar", src: "searchBar.jpg", link:"https://ana-portfolio-vanillajs.web.app/searchBar/"},
+    {name: "Slide Effect", src: "slideEffect.jpg", link:"https://ana-portfolio-vanillajs.web.app/slideEffect/"},
+    {name: "Tapas List", src: "tapasList.jpg", link:"https://ana-portfolio-vanillajs.web.app/tapasList/"},
+    {name: "Pick a Voice", src: "voiceChanger.jpg", link:"https://ana-portfolio-vanillajs.web.app/voiceChanger/"}
   ]
 }
