ac565a2a186d0739ce05f63007f56313e0617da9
Anto P <ganto1812@gmail.com>
2022-06-22 20:30:24 +0100

    Languages and avatar updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,20 +1,44 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   home = {
-    langs:["html.svg", "css.svg", "js.svg", "cSharp.svg"],
-    libs:["jquery.png", "reactjs.png", "angular.png", "bootstrap.png", "sass.png"],
-    designs:["photoshop.svg", "illustrator.jpg", "adobe-xd.svg", "gimp.png", "inkscape.png"],
-    others: ["github.png","bitbucket.svg", "slack.png","axosoft.svg", "icescrum.png", "figma.png"]
-  }
+    langs: ['html.svg', 'css.svg', 'js.svg', 'typescript.png', 'sql.png'],
+    libs: [
+      'reactjs.png',
+      'redux.png',
+      'angular.png',
+      'rxjs.png',
+      'bootstrap.png',
+      'tailwind.png',
+      'sass.png',
+      'jquery.png',
+    ],
+
+    others: [
+      'docker.png',
+      'kubernetes.png',
+      'github.png',
+      'bitbucket.webp',
+      'slack.png',
+      'axosoft.png',
+      'icescrum.png',
+    ],
+    testing: ['jest.png', 'react-testing-library.png', 'cypress.png'],
+    designs: [
+      'photoshop.svg',
+      'illustrator.jpg',
+      'adobe-xd.svg',
+      'gimp.png',
+      'inkscape.png',
+      'figma.svg',
+    ],
+  };
   constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
